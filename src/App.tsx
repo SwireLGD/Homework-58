6c331730a5ae07bd00e365a92a45502ac10d7468
Arsen Chernov <arsenchernov02@gmail.com>
2024-01-23 16:42:38 +0600

    Added Alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import Appbar from './Components/Appbar/Appbar';
 import MessagesList from './Components/MessageList/MessageList';
+import Alert from './Components/Alert/Alert';
 
 const App: React.FC = () => {
   const [messages, setMessages] = useState<string[]>(['Message 1', 'Message 2', 'Message 3']);
+  const [showAlert, setShowAlert] = useState(true);
+
   const addMessage = (message: string) => {
     setMessages((prevMessages) => [...prevMessages, message]);
+  };
+
+  const closeAlert = () => {
+    setShowAlert(false);
   };
 
   return (
@@ -18,6 +25,17 @@ const App: React.FC = () => {
           <h1>Messages</h1>
           <MessagesList messages={messages} addMessage={addMessage} />
         </div>
+        <Alert
+          type="warning"
+          onDismiss={closeAlert}
+          show={showAlert}>
+            This is a warning type alert
+        </Alert>
+        <Alert
+          type="success"
+          show={showAlert}>
+            This is a success type alert
+        </Alert>
       </main>
     </>
   );
---
+++ src/Components/Alert/Alert.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface Props extends React.PropsWithChildren{
+    type: "primary" | "success" | "danger" | "warning";
+    onDismiss?: () => void;
+    show: boolean;
+};
+
+const Alert: React.FC<Props> = ({ type, onDismiss, show, children }) => {
+   const alertType = `alert alert-${type}`;
+
+   return (
+    <>
+    {show && (
+        <div className={`mt-5 d-flex justify-content-between ${alertType}`} role="alert">
+            {children}
+            {onDismiss && (
+                <button
+                type="button" 
+                className="btn-close d-inline-block"
+                onClick={onDismiss}
+                >
+                </button>
+            )}
+        </div>
+    )}
+    </>
+   );
+};
+
+export default Alert
